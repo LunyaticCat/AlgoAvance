da019dd03a6f1710c6ba15ae74f7427c78e5cce4
LunaticCat <yannbrun1507@outlook.fr>
2021-09-21 11:16:20 +0200

    un done, started baguenaudier

--- src/main/java/TP_jeux_recursifs/Hanoï.java
+++ src/main/java/TP_jeux_recursifs/Hanoï.java
@@ -2,24 +2,32 @@ package TP_jeux_recursifs;
 
 public class Hanoï {
     public static void main(String[] args) {
-        resoudre(8);
+        System.out.println(calculerUn(3));
     }
 
     public static void resoudre(int n){
-        resoudreAux(n, 1, 3, 0);
+        resoudreAux(n, 1, 3);
     }
 
-    public static void resoudreAux(int n, int i, int j, int un){
+    public static void resoudreAux(int n, int i, int j){
         int k = 6 - i - j;
         if(n == 1){
             System.out.println(i + " -> " + j);
-            un++;
         }
         if(n>1) {
-            resoudreAux(n-1, i, k, un+1);
+            resoudreAux(n-1, i, k);
             System.out.println(i + " -> " + j);
-            un++;
-            resoudreAux(n-1, k, j, un+1);
+            resoudreAux(n-1, k, j);
+        }
+    }
+
+    public static int calculerUn(int n){
+        if(n == 1){
+            return 1;
+        }
+        else {
+            int x = calculerUn(n-1);
+            return 2*x+1;
         }
     }
 }
